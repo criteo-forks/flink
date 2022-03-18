054a8c2348872c13c6035e54c9e069deaafbcb49
Hervé RIVIERE <HerveRiviere@users.noreply.github.com>
2022-03-18 15:39:25 +0100

    Fix indentation of MetricUtil

--- flink-runtime/src/main/java/org/apache/flink/runtime/metrics/util/MetricUtils.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/metrics/util/MetricUtils.java
@@ -342,9 +342,9 @@ public class MetricUtils {
                     (com.sun.management.OperatingSystemMXBean)
                             ManagementFactory.getOperatingSystemMXBean();
             // In a containerized environment, the "system cpu load" is the load regarding to container's available processors.
-			// Whilst "process cpu load" is always computed against all cpus of the host.
-			metrics.<Double, Gauge<Double>>gauge("SystemLoad", mxBean::getSystemCpuLoad);
-			metrics.<Double, Gauge<Double>>gauge("ProcessLoad", mxBean::getProcessCpuLoad);
+            // Whilst "process cpu load" is always computed against all cpus of the host.
+            metrics.<Double, Gauge<Double>>gauge("SystemLoad", mxBean::getSystemCpuLoad);
+            metrics.<Double, Gauge<Double>>gauge("ProcessLoad", mxBean::getProcessCpuLoad);
             metrics.<Long, Gauge<Long>>gauge("Time", mxBean::getProcessCpuTime);
         } catch (Exception e) {
             LOG.warn(
